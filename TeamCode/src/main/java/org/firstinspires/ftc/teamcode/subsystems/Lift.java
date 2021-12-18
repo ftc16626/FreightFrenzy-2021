3cf7a1312328a7fba6272c21fc0dfa6ecb8b05cf
Devin34 <halld35n@gmail.com>
2021-12-18 04:10:10 -0600

    feat(Lift.java): Added the controls and logic for bucket servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -10,6 +11,7 @@ public class Lift {
 
     //Hardware
     public Servo liftServo; //Port 1-CH
+    public Servo bucketServo; //Port 1-CH
     private Servo tiltServoOne; //Port 0-CH
     private Servo tiltServoTwo; //Port 4-EH
     private CRServo gateServo;//Port 5-EH
@@ -26,35 +28,94 @@ public class Lift {
         return position;
     }
 
-    private double position;
+    private double position = .8;
+    public double bucketPosition = .1;
     private int changed = 0;
 
-    public void elevate(boolean a, boolean b, boolean y, double left_stick_y) {
+    public void elevate(boolean a, boolean b, boolean y, boolean x, double left_stick_y, float intake) {
         if (a && changed != 1) {
-            if (position != 0.5) position = 0.5;
-            else position = 0;
+            if (position != .5) {
+                position = .5;
+
+                bucketPosition = .4;
+            }
+            else {
+                position = .8;
+                bucketPosition = .1;
+            }
             changed = 1;
         } else if (b && changed != 2) {
-            if (position != 0.7) position = 0.7;
-            else position = 0;
+            if (position != 0.3) {
+                position = 0.3;
+                bucketPosition = .6;
+            }
+            else {
+                position = .8;
+                bucketPosition = .1;
+            }
             changed = 2;
         } else if (y && changed != 3) {
-            if (position != 1.0) position = 1.0;
-            else position = 0;
-            changed = 3;
-        }/*
-            else if(-gamepad1.left_stick_y > 0 && changed != 4){
-                position = -gamepad1.left_stick_y * 3000;
-                changed = 0;
+            if (position != .15) {
+                position = .15;
+                bucketPosition = .8;
             }
-            */ else if (!a && !b && !y) {
+            else {
+                position = .8;
+                bucketPosition = .1;
+            }
+            changed = 3;
+        }else if (x && changed != 4) {
+            if (bucketPosition != 1) {
+                bucketPosition = 1;
+            } else {
+                if (position == .5) {
+                    bucketPosition = .4;
+                }
+
+                if (position == .3) {
+                    bucketPosition = .6;
+                }
+
+                if (position == .15) {
+                    bucketPosition = .8;
+                }
+                if (position == .8) {
+                    bucketPosition = .1;
+                }
+
+            }
+            changed = 4;
+        } else if (!a && !b && !y && !x) {
+
             changed = 0;
 
         }
 
-        if (left_stick_y != 0) liftServo.setPosition(left_stick_y);
-        else liftServo.setPosition(position);
-    }
+
+
+            liftServo.setPosition(position);
+            if (intake < 0) {
+                bucketPosition = 0;
+
+            } else if (!x) {
+                if (position == .5) {
+                    bucketPosition = .4;
+                }
+
+                if (position == .3) {
+                    bucketPosition = .6;
+                }
+
+                if (position == .15) {
+                    bucketPosition = .8;
+                }
+                if (position == .8) {
+                    bucketPosition = .1;
+                }
+            }
+            bucketServo.setPosition(bucketPosition);
+        }
+    
 
 //    public enum Stage {
 //        DEFAULT, STAGEZERO, STAGEONE, STAGETWO, STAGETHREE
@@ -66,6 +127,9 @@ public class Lift {
 
     public void init(HardwareMap hardwareMap) {
         liftServo = hardwareMap.get(Servo.class, "liftServo");
+        bucketServo = hardwareMap.get(Servo.class, "bucketServo");
+       // bucketServo.setDirection(DcMotorSimple.Direction.REVERSE);
+
         //liftServo.setDirection(Servo.Direction.REVERSE);
         //tiltServoOne = hardwareMap.get(Servo.class, "tiltServoOne");
         //tiltServoTwo = hardwareMap.get(Servo.class, "tiltServoTwo");
